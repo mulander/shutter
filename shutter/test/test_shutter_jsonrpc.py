9cc5463546b03f9c26fb22f6d1b0e2ed57565579
mulander <netprobe@gmail.com>
2013-11-01 18:23:02 +0100

    Adding a test case to expose a bug in snapshots
    We made some changes bu is our service working correctly? Our previous test case passed but it worked with an empty database. Now we will extend our tests with a new case.
    
    We will import our config file and the psycopg2 module into the test suite and modify setUp
    to include initialization of a psycopg2 database connection and a cursor out of it.
    
    We will also modify the tearDown method to close the connection after the test finishes.
    
    Note this change will impact all test cases being run - even if they don't utilize the connection it will be established and closed upon each test.
    
    Next we will add a new test_snapshots_fake_entries in which we will insert directly into the database
    using our new connection. We have to commit the changes after each snapshot entry because postgres will set the created_at field to a time dependent upon when the changes were committed. If we do a single commit all the timestamps will be the same for our new entries.
    
    Running the new test case exposes that the return value is in a different format then expected:
    twisted.trial.unittest.FailTest: not equal:
    a = [[u'fake3.png'], [u'fake2.png'], [u'fake1.png']]
    b = ['fake3.png', 'fake2.png', 'fake1.png']
    
    shutter.test.test_shutter_jsonrpc.ShutterJSONRPCTestCase.test_snapshots_fake_entries
    
    The values returned by txn.fetchall() are additionally nested.

--- shutter/test/test_shutter_jsonrpc.py
+++ shutter/test/test_shutter_jsonrpc.py
@@ -5,16 +5,46 @@ from txjsonrpc.web.jsonrpc import Proxy
 from txjsonrpc import jsonrpclib
 
 from shutter.protocol import RemoteShutterFactory, ShutterJSONRPCProtocol
+from shutter import config
+
+import psycopg2
 
 class ShutterJSONRPCTestCase(unittest.TestCase):
     def setUp(self):
         self.port = reactor.listenTCP(8001, RemoteShutterFactory(ShutterJSONRPCProtocol()))
         self.client = Proxy('http://127.0.0.1:8001', version=jsonrpclib.VERSION_2)
+
+        self.conn = psycopg2.connect(config.DB_CONN)
+        self.cur  = self.conn.cursor()
     def tearDown(self):
         self.port.stopListening()
+        self.conn.close()
     def test_snapshots_empty(self):
         def _result(value):
             self.assertTrue(value['error'] is None)
             self.assertFalse(value['result'] is None)
             self.assertEqual(value['result'], [])
         return self.client.callRemote("snapshots", "http://programistamag.pl").addCallback(_result)
+    def test_snapshots_fake_entries(self):
+        def _result(value):
+            # Cleanup the added entries
+            self.cur.execute("DELETE FROM shutter.urls")
+            self.conn.commit()
+            # Check the return values
+            self.assertEqual(value['result'], ["fake3.png", "fake2.png", "fake1.png"])
+        # Add fake entries into the database
+        url = "http://fake.test.com"
+        self.cur.execute("""INSERT INTO shutter.urls(url)
+                                 VALUES (%s) 
+                              RETURNING id
+        """, [url])
+        url_id = self.cur.fetchone()[0]
+        def _make_file(fakeFile):
+            self.cur.execute("""INSERT INTO shutter.snapshots(url_id, file_path)
+                                     VALUES (%s, %s)""", [url_id, fakeFile])
+            self.conn.commit()
+        _make_file("fake1.png")
+        _make_file("fake2.png")
+        _make_file("fake3.png")
+
+        return self.client.callRemote("snapshots", "http://fake.test.com").addCallback(_result)
